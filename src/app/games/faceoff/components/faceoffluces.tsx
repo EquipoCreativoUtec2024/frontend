9f833ce36781f6434459aaf9931d2d2382966cb2
Gonzalo Perea <gonzalopereabartra@gmail.com>
2024-06-25 21:30:55 -0500

    feat: give points to user when finishing faceoff

--- src/app/games/faceoff/components/faceoffluces.tsx
+++ src/app/games/faceoff/components/faceoffluces.tsx
@@ -1,33 +1,45 @@
 "use client";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
 
-const colors = ["red", "green", "blue", "yellow", "purple", "orange", "pink", "skyblue"];
+const colors = [
+  "red",
+  "green",
+  "blue",
+  "yellow",
+  "purple",
+  "orange",
+  "pink",
+  "skyblue",
+];
 
 interface ColorsSpanish {
   [key: string]: string;
 }
 
 const colorsSpanish: ColorsSpanish = {
-  "red": "Rojo",
-  "green": "Verde",
-  "blue": "Azul",
-  "yellow": "Amarillo",
-  "purple": "Morado",
-  "orange": "Naranja",
-  "pink": "Rosado",
-  "skyblue": "Celeste"
+  red: "Rojo",
+  green: "Verde",
+  blue: "Azul",
+  yellow: "Amarillo",
+  purple: "Morado",
+  orange: "Naranja",
+  pink: "Rosado",
+  skyblue: "Celeste",
 };
 
 const winningColor = colors[Math.floor(Math.random() * colors.length)];
 const winningColorEs = colorsSpanish[winningColor as string];
 
 const FaceoffLuces = () => {
+  const router = useRouter();
   const [showColorText, setShowColorText] = useState(true);
   const [currentColor, setCurrentColor] = useState("");
   const [winner, setWinner] = useState("");
   const [shotMessage, setShotMessage] = useState("");
   const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
-  const [prevWinningColor, setPrevWinningColor] = useState('');
+  const [prevWinningColor, setPrevWinningColor] = useState("");
 
   useEffect(() => {
     setPrevWinningColor(winningColor);
@@ -50,56 +62,123 @@ const FaceoffLuces = () => {
     if (currentColor === winningColor) {
       setWinner(player);
       setShotMessage(`Jugador ${player} ganó`);
-      setShotMessage((prevMessage) => `${prevMessage} Jugador ${player === '1' ? '2' : '1'}: shot!`);
+      setShotMessage(
+        (prevMessage) =>
+          `${prevMessage} Jugador ${player === "1" ? "2" : "1"}: shot!`
+      );
     } else {
-      setWinner(player === '1' ? '2' : '1');
-      setShotMessage(`Jugador ${player === '1' ? '2' : '1'} ganó`);
-      setShotMessage((prevMessage) => `${prevMessage} Jugador ${player}: shot!`);
+      setWinner(player === "1" ? "2" : "1");
+      setShotMessage(`Jugador ${player === "1" ? "2" : "1"} ganó`);
+      setShotMessage(
+        (prevMessage) => `${prevMessage} Jugador ${player}: shot!`
+      );
     }
   };
+
+  const handleNewGame = () => {
+    const userData = Cookies.get("userData");
+    if (!userData || userData === "undefined") {
+      router.push("/auth");
+      return;
+    }
+    const username = JSON.parse(userData).username;
+    fetch("https://8zpbnlo7dd.execute-api.us-east-1.amazonaws.com/dev/user", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+      }),
+    });
+    router.push("/games/faceoff/faceoffjuego");
+  };
+
+  const volverAlInicio = () => {
+    const userData = Cookies.get("userData");
+    if (!userData || userData === "undefined") {
+      router.push("/auth");
+      return;
+    }
+    const username = JSON.parse(userData).username;
+    fetch("https://8zpbnlo7dd.execute-api.us-east-1.amazonaws.com/dev/user", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+      }),
+    });
+    router.push("/");
+  }
 
   return (
     <div className="h-screen bg-gray100 flex flex-col items-center justify-center">
       {showColorText ? (
         <div className="text-center">
           <div className="text-4xl font-bold text-white mb-4">El color es:</div>
-          <div className="text-6xl font-bold" style={{ color: prevWinningColor }}>{prevWinningColor}</div>
+          <div
+            className="text-6xl font-bold"
+            style={{ color: prevWinningColor }}
+          >
+            {prevWinningColor}
+          </div>
         </div>
       ) : winner ? (
         <div className="text-4xl font-bold text-center">
-          <div style={{
-            backgroundColor: '#2D0AFF',
-            color: 'white',
-            padding: '30px',
-            borderRadius: '20px',
-            marginBottom: '20px',
-            fontSize: '27px',
-            display: 'flex',
-            flexDirection: 'column',
-            textAlign: 'center',
-            fontFamily: 'Verdana',
-            width: '320px',
-          }}>
-            <p>{shotMessage}</p>
-          </div>
-          <div style={{ marginBottom: '40px' }}>
-            {}
-          </div>
-          <button
+          <div
             style={{
-              marginTop: '10px',
-              padding: '8px 18px',
-              fontSize: '16px',
-              backgroundColor: 'white',
-              color: 'black',
-              border: 'none',
-              borderRadius: '5px',
-              cursor: 'pointer',
-              fontWeight: 'bold',
-              boxShadow: '0px 2px 6px rgba(0, 0, 0, 0.3)',
+              backgroundColor: "#2D0AFF",
+              color: "white",
+              padding: "30px",
+              borderRadius: "20px",
+              marginBottom: "20px",
+              fontSize: "27px",
+              display: "flex",
+              flexDirection: "column",
+              textAlign: "center",
+              fontFamily: "Verdana",
+              width: "320px",
             }}
           >
-            <a href="./faceoffluces">Otra partida</a>
+            <p>{shotMessage}</p>
+          </div>
+          <div style={{ marginBottom: "40px" }}>{}</div>
+          <button
+            style={{
+              marginTop: "10px",
+              padding: "8px 18px",
+              fontSize: "16px",
+              backgroundColor: "white",
+              color: "black",
+              border: "none",
+              borderRadius: "5px",
+              cursor: "pointer",
+              fontWeight: "bold",
+              boxShadow: "0px 2px 6px rgba(0, 0, 0, 0.3)",
+            }}
+            onClick={handleNewGame}
+          >
+            Otra partida
+          </button>
+          <button
+            style={{
+              marginTop: "10px",
+              marginLeft: "10px",
+              padding: "8px 18px",
+              fontSize: "16px",
+              backgroundColor: "white",
+              color: "black",
+              border: "none",
+              borderRadius: "5px",
+              cursor: "pointer",
+              fontWeight: "bold",
+              boxShadow: "0px 2px 6px rgba(0, 0, 0, 0.3)",
+            }}
+            onClick={volverAlInicio}
+          >
+            Volver al inicio
           </button>
         </div>
       ) : (
@@ -107,15 +186,17 @@ const FaceoffLuces = () => {
           <button
             className="w-full h-1/2 flex items-center justify-center"
             style={{ backgroundColor: currentColor }}
-            onClick={() => handleClick('1')}
+            onClick={() => handleClick("1")}
           >
-            <span className="text-2xl font-bold text-white transform rotate-180">Jugador 1</span>
+            <span className="text-2xl font-bold text-white transform rotate-180">
+              Jugador 1
+            </span>
           </button>
           <div className="w-full h-1 border-[6px] bg-white"></div>
           <button
             className="w-full h-1/2 flex items-center justify-center"
             style={{ backgroundColor: currentColor }}
-            onClick={() => handleClick('2')}
+            onClick={() => handleClick("2")}
           >
             <span className="text-2xl font-bold text-white">Jugador 2</span>
           </button>
